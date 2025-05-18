ea0f2bcef2a2d0ff3d351a2249f6668044f46cef
andreykinn20 <andreykinn20@gmail.com>
2025-05-18 20:19:02 +0400

    Slice tests

--- taxi-fleet-server/src/test/java/com/andreine/taxifleet/controller/BaseControllerTest.java
+++ taxi-fleet-server/src/test/java/com/andreine/taxifleet/controller/BaseControllerTest.java
@@ -1,13 +1,15 @@
 package com.andreine.taxifleet.controller;
 
 import com.andreine.taxifleet.TaxiFleetTestApplication;
+import com.andreine.taxifleet.service.BookingService;
 import com.andreine.taxifleet.service.TaxiBookingManagementService;
+import com.andreine.taxifleet.service.TaxiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(TaxiFleetTestApplication.class)
+@WebMvcTest
 public class BaseControllerTest {
 
     @Autowired
@@ -15,5 +17,11 @@ public class BaseControllerTest {
 
     @MockitoBean
     protected TaxiBookingManagementService taxiBookingManagementService;
+
+    @MockitoBean
+    protected BookingService bookingService;
+
+    @MockitoBean
+    protected TaxiService taxiService;
 
 }
--- taxi-fleet-server/src/test/java/com/andreine/taxifleet/controller/publicapi/PublicApiControllerTest.java
+++ taxi-fleet-server/src/test/java/com/andreine/taxifleet/controller/publicapi/PublicApiControllerTest.java
@@ -1,16 +1,33 @@
 package com.andreine.taxifleet.controller.publicapi;
 
 import com.andreine.taxifleet.controller.BaseControllerTest;
+import com.andreine.taxifleet.exception.BookingNotFoundException;
+import com.andreine.taxifleet.exception.IllegalBookingStatusException;
+import com.andreine.taxifleet.exception.IllegalTaxiStatusException;
 import com.andreine.taxifleet.exception.TaxiNotFoundException;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class PublicApiControllerTest extends BaseControllerTest {
+
+    @Test
+    @SneakyThrows
+    void shouldHandleRuntimeException() {
+        when(bookingService.getAvailableBookings()).thenThrow(new RuntimeException("Error occurred"));
+
+        mockMvc.perform(get("/public/bookings/available"))
+            .andExpect(status().isInternalServerError())
+            .andExpect(jsonPath("$.message", containsString("Error occurred")));
+    }
 
     @Test
     @SneakyThrows
@@ -19,7 +36,41 @@ class PublicApiControllerTest extends BaseControllerTest {
 
         mockMvc.perform(post("/public/taxi/{taxiId}/bookings/{bookingId}/accept", 1L, 1L)
                 .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isBadRequest());
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message", containsString("Taxi 1 is not found")));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldHandleIllegalTaxiStatusException() {
+        doThrow(new IllegalTaxiStatusException("Illegal taxi status")).when(taxiBookingManagementService).acceptBooking(1L, 1L);
+
+        mockMvc.perform(post("/public/taxi/{taxiId}/bookings/{bookingId}/accept", 1L, 1L)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message", containsString("Illegal taxi status")));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldHandleBookingNotFoundException() {
+        doThrow(new BookingNotFoundException(1L)).when(bookingService).cancelBooking(1L);
+
+        mockMvc.perform(post("/public/bookings/{bookingId}/cancel", 1L)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message", containsString("Booking 1 is not found")));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldHandleIllegalBookingStatusException() {
+        doThrow(new IllegalBookingStatusException("Illegal booking status")).when(bookingService).cancelBooking(1L);
+
+        mockMvc.perform(post("/public/bookings/{bookingId}/cancel", 1L)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message", containsString("Illegal booking status")));
     }
 
 }
